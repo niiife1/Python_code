21bd8aad61187f3c31e6a7a16d89d651aebbd2ed
niiife1 <57578406+niiife1@users.noreply.github.com>
2022-07-18 23:55:37 +0300

    Add files via upload

---
+++ lagrest_number.py
@@ -0,0 +1,13 @@
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+if num1 > num2:
+    if num1 > num3:
+     print(num1)
+if num2 > num1:
+    if num2 > num3:
+        print(num2)
+if num3 > num1:
+    if num3 > num2:
+        print(num3)
