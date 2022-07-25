812c54ebf086ad2e67da593d03388c01e54aec8b
niiife1 <57578406+niiife1@users.noreply.github.com>
2022-07-25 22:32:18 +0300

    Add files via upload

---
+++ user.py
@@ -0,0 +1,30 @@
+
+class Vet:
+    animals = []
+    space = 5
+    def __init__(self,name):
+        self.name = name
+        self.animals = []
+        
+    @property
+    def animal_number():
+        return "tuk printiram"
+    
+    def register_animal(self,animal_name):
+        if len(Vet.animals) < Vet.space:
+            self.animals.append(animal_name)
+            Vet.animals.append(animal_name)
+            return f"{animal_name} registered in the clinic"
+        return "Not enough space"
+    def unregister_animal(self,animal_name):
+            if animal_name in Vet.animals:
+                Vet.animals.remove(animal_name)
+                self.animals.remove(animal_name)
+                return f"{animal_name} unregistered successfully"
+            return f"{animal_name} not in the clinic"
+    def info(self):
+        return f"{self.name} has {len(self.animals)} animals. {Vet.space - len(Vet.animals)} space left in clinic"
+
+peter = Vet("Peter")
+george = Vet("George")
+print(Vet.animal_number())
